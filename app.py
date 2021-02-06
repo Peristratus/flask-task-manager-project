79cb84ad8b7faf3854daa1990dffc0a6161e5cc7
Peristratus <testcorp24@gmail.com>
2021-02-06 04:40:38 +0000

    code for login and authentication inserted

--- app.py
+++ app.py
@@ -47,6 +47,33 @@ def register():
     return render_template("register.html")
 
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method =="POST":
+        # check if the username exist in the db
+        existing_user = mongo.db.users.find_one(
+            {"username":request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hased password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("incorrect username and/or password")
+                return redirect(url_for("login"))
+        else:
+            # user doesn't exist
+            flash("incorrect Username and/or password ")
+            return redirect("login")
+
+
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
